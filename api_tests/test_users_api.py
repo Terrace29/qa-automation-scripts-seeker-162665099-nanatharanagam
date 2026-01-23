b69b56a7758d7726700d10acdf489a9a1c615314
lavanya <lavanyareddy1523@gmail.com>
2026-01-23 07:50:11 -0500

    Add api test scripts

---
+++ api_tests/test_users_api.py
@@ -0,0 +1,12 @@
+import requests
+
+def test_get_users_api():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+    # validate status code
+    assert response.status_code == 200
+
+    #validate response body
+    data = response.json()
+    assert isinstance(data,list)
+    assert len(data) > 0
