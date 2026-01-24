e84b311634a9cc1c23993d3df9fa0c98087fe23d
lavanya <lavanyareddy1523@gmail.com>
2026-01-24 06:58:46 -0500

    Nested loop

---
+++ Python_prac/Nested loop.py
@@ -0,0 +1,5 @@
+for x in range(2):
+    print("The is the outer loop iteration number" +str(x))
+    for y in range(3+1):
+        print("This is the inner loop iteration number" +str(y))
+    print("Exit inner loop")
